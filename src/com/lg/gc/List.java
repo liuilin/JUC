25568f797151137ae092eade61149ec11d20f152
iMugen <liu1142865396@gmail.com>
2020-02-25 15:59:44 +0800

    init-commit

--- src/com/lg/gc/List.java
+++ src/com/lg/gc/List.java
@@ -10,15 +10,14 @@ import java.util.stream.Collectors;
  * @author Daniel Liu 2019/7/2 19:52
  */
 public class List {
-    public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("a", "b", "c"));
-        System.out.println(list);
+    public static void main(String[] args) throws Exception {
+
+        java.util.List list = new ArrayList<String>();
 
         //必须传一个对象，而非int数组（推荐java8的stream方式）
         System.out.println(Arrays.stream(new Integer[]{1, 2, 3}).collect(Collectors.toList()));
-        System.out.println("hhh,submittttt");
-        System.out.println("test");
-        System.out.println("12345");
-        System.out.println("123456");
+//        (() -> System.out.println("shit"));
+
+
     }
 }
