c606822f2f696163a75e690805757e01bf80ed1f
liuqiang <liu1142865396@gmail.com>
2021-07-10 20:22:25 +0800

    update

--- src/main/java/com/liumulin/LockSupportDemo.java
+++ src/main/java/com/liumulin/LockSupportDemo.java
@@ -1,5 +1,9 @@
 package com.liumulin;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author liuqiang
  * @since 2021-07-10
@@ -7,9 +11,47 @@ package com.liumulin;
 public class LockSupportDemo {
 
     private static Object objLock = new Object();
+    private static ReentrantLock rLock = new ReentrantLock();
+    private static Condition condition = rLock.newCondition();
 
     public static void main(String[] args) {
+    }
+
+    private static void conditionAwaitSignal() {
         new Thread(() -> {
+            rLock.lock();
+            try {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "---come in");
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } finally {
+                rLock.unlock();
+            }
+            System.out.println(Thread.currentThread().getName() + "---被唤醒");
+        }, "A").start();
+
+        new Thread(() -> {
+            rLock.lock();
+            try {
+                condition.signal();
+                System.out.println(Thread.currentThread().getName() + "---通知");
+            } finally {
+                rLock.unlock();
+            }
+        }, "B").start();
+    }
+
+    private static void synchronizedWaitNotify() {
+        new Thread(() -> {
+            //暂停一会儿线程
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             synchronized (objLock) {
                 System.out.println(Thread.currentThread().getName() + "---come in");
                 try {
@@ -17,14 +59,14 @@ public class LockSupportDemo {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName()+"---被唤醒");
+                System.out.println(Thread.currentThread().getName() + "---被唤醒");
             }
         }, "A").start();
 
         new Thread(() -> {
             synchronized (objLock) {
                 objLock.notify();
-                System.out.println(Thread.currentThread().getName()+"---通知");
+                System.out.println(Thread.currentThread().getName() + "---通知");
             }
         }, "B").start();
     }
